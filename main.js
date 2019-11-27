b3980ab7744f13a4144b900d57f17f5a62e1bed7
andruraj <andrewraj10@gmail.com>
2019-11-27 16:57:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3");
-
-class DataBase {
-  constructor() {
+let db = new sqlite3.Database("newDB");
+class SmallDB {
+  create() {
     // open the database connection
     let db = new sqlite3.Database("newDB", err => {
       if (err) {
@@ -11,19 +11,20 @@ class DataBase {
 
     db.serialize(() => {
       // Queries scheduled here will be serialized.
-      db.run("CREATE TABLE IF NOT EXISTS greetings(message text)")
-        .run(
-          `INSERT INTO greetings(message)
-          VALUES('Hi'),
-                ('Hello'),
-                ('Welcome')`
-        )
-        .each(`SELECT message FROM greetings`, (err, row) => {
-          if (err) {
-            throw err;
-          }
-          console.log(row.message);
-        });
+      db.run(
+        `CREATE TABLE IF NOT EXISTS projects(
+          id INT PRIMARY KEY AUTOINCREMENT,
+          name TEXT NOT NULL,
+          desc TEXT,
+          completed NUMERIC DEFAULT 0
+      )`
+      ).each(`SELECT * FROM projects`, (err, row) => {
+        if (err) {
+          throw err;
+          console.log(err);
+        }
+        console.log(row);
+      });
     });
 
     // close the database connection
@@ -33,6 +34,61 @@ class DataBase {
       }
     });
   }
+
+  getall() {
+    return new Promise((resolve, reject) => {
+      db.serialize(() => {
+        db.all(`select * from projects`, (err, rows) => {
+          if (err) {
+            // console.log(err);
+            reject(err);
+          } else {
+            // console.log(rows);
+            resolve(rows);
+          }
+        });
+      });
+    });
+  }
+
+  get(id) {
+    return new Promise((resolve, reject) => {
+      db.serialize(() => {
+        db.all(`select * from projects where id = ${id}`, (err, rows) => {
+          if (err) {
+            // console.log(err);
+            reject(err);
+          } else {
+            // console.log(rows);
+            resolve(rows);
+          }
+        });
+      });
+    });
+  }
+
+  add(name, desc, completed) {
+    return new Promise((resolve, reject) => {
+      db.serialize(() => {
+        db.run(
+          `INSERT INTO projects(name, desc, completed) VALUES('${name}', '${desc}', ${completed})`
+        ).all(
+          `SELECT * from projects where name = '${name}' and desc = '${desc}' and completed = ${completed}`,
+          (err, row) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(row);
+            }
+          }
+        );
+      });
+    });
+  }
+
+  delete(id){
+      
+  }
 }
 
-module.exports = DataBase;
+module.exports = SmallDB;
